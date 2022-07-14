ff0cf02f2c0934b53250cb4ccdf6f929e753daba
Alzy Welzy <welzyalzy@gmail.com>
2022-07-14 20:43:04 +0530

    finally it's done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+const allLinks = document.querySelectorAll("a:link");
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const main = document.querySelector(".main");
+const headerEl = document.querySelector(".header");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+  //   main.classList.toggle("none");
+});
+
 const yearElement = document.querySelector(".year");
 yearElement.textContent = new Date().getFullYear();
 
@@ -24,3 +34,34 @@ const observer = new IntersectionObserver(
 );
 
 observer.observe(sectionHeroEl);
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+
+    // Scroll to top
+    // Event caused by logo click
+    if (href === "#") {
+      // const target = this.getAttribute("href");
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    // Scroll to link/Section
+    // Event caused by link click
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionlEl = document.querySelector(href);
+      // console.log(sectionlEl);
+      sectionlEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("nav-link")) {
+      //   main.classList.toggle("none");
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
