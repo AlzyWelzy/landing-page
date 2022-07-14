440f8c99b646ab0f11c39b74e71220ccfe7aa856
Alzy Welzy <welzyalzy@gmail.com>
2022-07-14 17:39:07 +0530

    completed making sticky nav bar

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
 const yearElement = document.querySelector(".year");
 yearElement.textContent = new Date().getFullYear();
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const observer = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+
+observer.observe(sectionHeroEl);
